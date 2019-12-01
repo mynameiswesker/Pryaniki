d89b0c444b6308345858246fed97319d6cfbdb84
mynameiswesker <alexandr071@bk.ru>
2019-12-01 17:09:59 +0300

    Updates

---
+++ precache-manifest.3e05d22af82a68062ddb50740fee038a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479465092cf439ceb4fa0293e2f4e492",
+    "url": "/Pryaniki/index.html"
+  },
+  {
+    "revision": "2c7df3075bab19639c6d",
+    "url": "/Pryaniki/static/css/main.ca328605.chunk.css"
+  },
+  {
+    "revision": "4e5da68bf0c835c6137f",
+    "url": "/Pryaniki/static/js/2.4425f375.chunk.js"
+  },
+  {
+    "revision": "2c7df3075bab19639c6d",
+    "url": "/Pryaniki/static/js/main.827239ed.chunk.js"
+  },
+  {
+    "revision": "1726c0e84975b761b782",
+    "url": "/Pryaniki/static/js/runtime-main.5e3bbc48.js"
+  }
+]);
